c64a497b48af62197eba14bc4dc4772953abe0ef
Andrew <andystep333@gmail.com>
2024-08-27 21:48:49 +0300

    changed tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,18 @@
 import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
 
 export default config;
